0252cc3fa05db712232333560a72e1bf06e4e2b9
sheriff Oladimeji <dimejiademola5@gmail.com>
2024-01-01 10:47:27 +0100

    feat: add created row to todo model

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Todo
+
 # Register your models here.
-admin.site.register(Todo)
+
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ("title", "is_complete", "created")
+
+
+admin.site.register(Todo, TodoAdmin)
--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
 class Todo(models.Model):
     title = models.CharField(max_length=1000)
     is_complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add= True)
