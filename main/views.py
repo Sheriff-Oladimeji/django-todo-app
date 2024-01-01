6b27b2cb32fcd4fae3ed65b505e4e065902cb381
sheriff Oladimeji <dimejiademola5@gmail.com>
2024-01-01 10:12:21 +0100

    feat: add view for todos and main

--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def Main(request):
+    return render(request, "main.html")
+
+
+def Todos(request):
+    return render(request, "todos.html")
